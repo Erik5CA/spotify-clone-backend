9047a82c3388419e5b681bf081ec9e40f0836b26
Erik5CA <erik.castillo.anzures@gmail.com>
2024-11-18 13:14:27 -0600

    added controller to add user to database

--- src/routes/song.route.js
+++ src/routes/song.route.js
@@ -1,27 +1,9 @@
 import { Router } from "express";
-import User from "../models/user.model.js";
 
 const router = Router();
 
-router.post("/callback", async (req, res) => {
-  try {
-    const { id, firstName, lastName, imageUrl } = req.body;
-    // check if user exists
-    const user = await User.findOne({ clerkId: id });
-    if (!user) {
-      // create user
-      const newUser = new User({
-        clerkId: id,
-        fullName: `${firstName} ${lastName}`,
-        imageUrl,
-      });
-      await newUser.save();
-    }
-    res.status(200).json({ success: true });
-  } catch (error) {
-    console.log("Error in callback: ", error);
-    res.status(500).json({ message: "Internal Server Error", error });
-  }
+router.get("/", (req, res) => {
+  res.send("Song Route with get method");
 });
 
 export default router;
