920f84b279c679bf578257d5d2640ea91f492d80
Erik5CA <erik.castillo.anzures@gmail.com>
2024-11-18 13:34:00 -0600

    added middleware to handle authorization

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import { clerkMiddleware } from "@clerk/express";
+
 import userRoutes from "./routes/user.route.js";
 import adminRoutes from "./routes/admin.route.js";
 import authRoutes from "./routes/auth.route.js";
@@ -13,6 +15,8 @@ dotenv.config();
 const app = express();
 
 app.use(express.json()); // to parse JSON bodies
+
+app.use(clerkMiddleware());
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
